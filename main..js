ba0a7292b6bcb6aa433d9725ad24532b01bd9026
sa20122424 <saloxiddin201224@gmail.com>
2024-09-10 18:01:14 +0500

    salom

--- main..js
+++ main..js
@@ -10,16 +10,27 @@ let data1 = [201212]
 
 alert(`sizningi Login ${data} va pasword ${data1}`)
 
-btn.addEventListener("click",()=>{
-    if (inp.value == data && inp2.value == data1) {
-
-       
+btn.addEventListener("click",() =>{
+  if (inp.value == data && inp2.value == data1 ) {
+      alert(`Дабро пожаловать`)
+  
+  
+  
+       if (inp.value == data && inp2.value == data1 == true ) {
+        p.textContent = ""
+        // p.innerHTML = `<p style="color:white;font-size:15px;" >sizninig Login yoki Psaword toxri emas</p>`
+        box2.append(p)
+       p.style.fontSize = "25px"
+        p.style.color = "red"
+        p.style.width = "300px"
+        p.style.textAlign = "center"
+       }
         let p  = document.createElement("p")
-        alert(`Дабро пожаловать`)
       
         let h1 = document.querySelector(".h1")
         let btn1 = document.querySelector(".btn1")
-     helo.append(h1,btn1)   
+     
+    helo.append(h1,btn1)   
      helo.style.display = "block" 
      h1.style.color = "black"
      helo.style.display = "flex" 
@@ -29,6 +40,7 @@ btn.addEventListener("click",()=>{
      helo.style.gap ="20px"
      
      let helo2 = document.querySelector(".helo2")
+    
      helo.append(helo2)
      btn1.addEventListener("click",()=>{
          let p2 = document.createElement("p")
@@ -42,18 +54,23 @@ btn.addEventListener("click",()=>{
 
 
         box2.append(p)
-            inp2.value = ''
+      inp2.value = ''
       inp.value = ''
    
-    } else {
-        let p  = document.createElement("p")
-        p.innerHTML = `<p style="color:white;font-size:15px;" >sizninig ID yoki Psaword toxri emas</p>`
-        box2.append(p)
+    } 
+
+  
+    else  {
+    
+      p.textContent = "sizninig Login yoki Psaword toxri emas"
+      // p.innerHTML = `<p style="color:white;font-size:15px;" >sizninig Login yoki Psaword toxri emas</p>`
+      box2.append(p)
+     p.style.fontSize = "25px"
+      p.style.color = "red"
+      p.style.width = "300px"
+      p.style.textAlign = "center"
+   
         
-        box2.style.backgroundColor = "red"
-       p.style.color = "white"
-         box2.style.padding = "10px"
-         box2.style.borderRadius = "20px"
         
          let h1 = document.querySelector(".h1")
          let btn1 = document.querySelector(".btn1")
@@ -68,6 +85,7 @@ btn.addEventListener("click",()=>{
       
       let helo2 = document.querySelector(".helo2")
       helo.append(helo2)
+      
       btn1.addEventListener("click",()=>{
         
         helo.style.display = "flex" 
@@ -82,11 +100,12 @@ btn.addEventListener("click",()=>{
           helo2.append(data,p2)
 
         
-      })
-
+        })
+        
         inp2.value = ''
-      inp.value = ''
-    }
-})
-
+        inp.value = ''
+      }
+    })
+    
+    let p  = document.createElement("p")
 
